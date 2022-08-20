92d9dfa965cdd62dcabf0a6b0cf2ed8a8f3a8cac
Femi-Ola Ilerioluwa <femiolailerioluwa@gmail.com>
2022-08-20 17:31:05 +0100

    ch1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 import "./App.css";
 import Header from "./pages/Header";
 import Body from "./pages/Body";
@@ -12,19 +16,22 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route index element={<Body />} />
-          <Route path="/" element={<Body />}></Route>
-          <Route path="/home" element={<Body />} />
-          <Route path="/devices" element={<Devices />} />
-          <Route path="/dictionary" element={<Dictionary />} />
-          <Route path="/devices/accordion" element={<Accordion />} />
-          <Route path="/devices/selected/:id" element={<Devicesinselectedgroup />} />          
-          <Route path="/devices/:id" element={<Accordion />} />
-          {/* <Route path="/editStaff/:id" element={<UpdateStaff />} /> */}
-        </Routes>        
-      <Footer />
+          <Header />
+          <Routes>
+            <Route index element={<Body />} />
+            <Route path="/" element={<Body />}></Route>
+            <Route path="/home" element={<Body />} />
+            <Route path="/devices" element={<Devices />} />
+            <Route path="/dictionary" element={<Dictionary />} />
+            <Route path="/devices/accordion" element={<Accordion />} />
+            <Route
+              path="/devices/selected/:id"
+              element={<Devicesinselectedgroup />}
+            />
+            <Route path="/devices/:id" element={<Accordion />} />
+            {/* <Route path="/editStaff/:id" element={<UpdateStaff />} /> */}
+          </Routes>
+          <Footer />
       </BrowserRouter>
     </>
   );
